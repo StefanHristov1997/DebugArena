4311c9af961eaa52109d3a52adacea88cb030565
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-10 13:14:45 +0300

    Add navigation responsive design.

--- src/main/java/com/debugArena/config/WebConfig.java
+++ src/main/java/com/debugArena/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
+        messageSource.setBasename("classpath:i18n/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
