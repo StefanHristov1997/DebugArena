5d07521601a4c3d89792f94a7a0534f5e2c52f3d
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-17 13:07:51 +0300

    Add checkingServerStatus method before invoke restClient.

--- src/main/java/com/debugArena/service/impl/EventServiceImpl.java
+++ src/main/java/com/debugArena/service/impl/EventServiceImpl.java
@@ -1,27 +1,39 @@
 package com.debugArena.service.impl;
 
+import com.debugArena.exeption.ServerConnectionException;
+import com.debugArena.exeption.ObjectNotFoundException;
 import com.debugArena.model.dto.binding.AddEventBindingModel;
 import com.debugArena.model.dto.view.EventDetailsInfoViewModel;
 import com.debugArena.model.dto.view.EventShortInfoViewModel;
 import com.debugArena.service.EventService;
+import com.debugArena.service.helpers.EventAPIServerStatusHelper;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService {
 
-    private RestClient restClient;
+    private final RestClient restClient;
+    private final EventAPIServerStatusHelper eventAPIServerStatusHelper;
 
-    public EventServiceImpl(RestClient restClient) {
+    public EventServiceImpl(
+            RestClient restClient,
+            EventAPIServerStatusHelper eventAPIServerStatusHelper) {
+
         this.restClient = restClient;
+        this.eventAPIServerStatusHelper = eventAPIServerStatusHelper;
     }
 
     @Override
     public void registerEvent(AddEventBindingModel addEventBindingModel) {
+
+        checkingServerStatus();
+
         restClient
                 .post()
                 .uri("/events")
@@ -31,27 +43,40 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public EventDetailsInfoViewModel getEventDetailsInfoById(Long id) {
-        return restClient
-                .get()
-                .uri("/events/{id}", id)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .body(EventDetailsInfoViewModel.class);
+
+        checkingServerStatus();
+
+        try {
+            return restClient
+                    .get()
+                    .uri("/events/{id}", id)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(EventDetailsInfoViewModel.class);
+        } catch (RestClientException e) {
+            throw new ObjectNotFoundException("Event with id " + id + " not found");
+        }
     }
 
     @Override
     public List<EventShortInfoViewModel> getEvents() {
-        return restClient
-                .get()
-                .uri("/events")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {
-                });
+
+        checkingServerStatus();
+
+            return restClient
+                    .get()
+                    .uri("/events")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
     }
 
     @Override
     public List<EventDetailsInfoViewModel> getWeeklyEvents() {
+
+        checkingServerStatus();
+
         return restClient
                 .get()
                 .uri("/events/weekly-events")
@@ -59,5 +84,12 @@ public class EventServiceImpl implements EventService {
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
+    }
+
+
+    private void checkingServerStatus() {
+        if (!eventAPIServerStatusHelper.isApiServerUp()){
+            throw new ServerConnectionException();
+        }
     }
 }
