b322df25a9b085f94acd17beca7db71900cc7bda
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-24 14:43:03 +0300

    Add integration tests.

--- src/test/java/com/debugArena/web/UserProfileControllerIT.java
+++ src/test/java/com/debugArena/web/UserProfileControllerIT.java
@@ -1,17 +1,17 @@
 package com.debugArena.web;
 
-import com.debugArena.model.dto.binding.UserProfileBindingModel;
 import com.debugArena.model.entity.UserEntity;
 import com.debugArena.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,6 +27,11 @@ class UserProfileControllerIT {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    void clearDB() {
+        userRepository.deleteAll();
+    }
 
     @Test
     @WithMockUser(username = "shristov16@e-dnrs.org", authorities = {"view"})
@@ -47,5 +52,35 @@ class UserProfileControllerIT {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(model().attributeExists("userProfile"))
                 .andExpect(view().name("profile"));
+    }
+
+    @Test
+    @WithMockUser(username = "shristov16@e-dnrs.org", authorities = {"view"})
+    void testEditProfile() throws Exception {
+
+        UserEntity testUser = new UserEntity();
+        testUser.setUsername("Shristov 16");
+        testUser.setPassword("test123");
+        testUser.setEmail("shristov16@e-dnrs.org");
+        testUser.setSkills("");
+        testUser.setInterests("");
+        testUser.setDescription("");
+
+        userRepository.save(testUser);
+
+        mockMvc.perform(patch("/users/profile/edit-profile")
+                        .with(csrf())
+                .param("description", "test description123")
+                .param("skills", "test skills123")
+                .param("interests", "test interests123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/users/profile"));
+
+        Optional<UserEntity> userOpt = userRepository.findByEmail("shristov16@e-dnrs.org");
+
+        Assertions.assertTrue(userOpt.isPresent());
+        Assertions.assertEquals("test description123" , userOpt.get().getDescription());
+        Assertions.assertEquals("test skills123" , userOpt.get().getSkills());
+        Assertions.assertEquals("test interests123" , userOpt.get().getInterests());
     }
 }
