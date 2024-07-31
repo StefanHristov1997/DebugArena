060a3e5575430303f3f86425da942e21b91a2af9
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-31 15:51:52 +0300

    Add CloudinaryService impl.

--- src/main/java/com/debugArena/service/impl/CloudinaryService.java
+++ src/main/java/com/debugArena/service/impl/CloudinaryService.java
@@ -3,6 +3,8 @@ package com.debugArena.service.impl;
 import com.cloudinary.Cloudinary;
 import com.debugArena.exeption.FuncErrorException;
 import com.debugArena.model.dto.binding.CloudinaryResponse;
+import com.debugArena.model.entity.UserEntity;
+import com.debugArena.service.helpers.LoggedUserHelper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,18 +17,23 @@ public class CloudinaryService {
 
     private final Cloudinary cloudinary;
 
+    private final LoggedUserHelper loggedUserHelper;
+
     @Autowired
-    public CloudinaryService(Cloudinary cloudinary) {
+    public CloudinaryService(Cloudinary cloudinary, LoggedUserHelper loggedUserHelper) {
         this.cloudinary = cloudinary;
+        this.loggedUserHelper = loggedUserHelper;
     }
 
     @Transactional
     public CloudinaryResponse uploadFile(final MultipartFile file, final String fileName) {
+        UserEntity currentUser = loggedUserHelper.get();
+
         try {
             final Map result = this.cloudinary.uploader()
                     .upload(file.getBytes(),
                             Map.of("public_id",
-                                    "users/settings/"
+                                    "DebugArena/userID/" + currentUser.getId() + "/"
                                             + fileName));
             final String url = (String) result.get("secure_url");
             final String publicId = (String) result.get("public_id");
