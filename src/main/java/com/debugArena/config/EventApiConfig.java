d780086db16a649e1bdcd8440e7c74628c7dc33a
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-31 12:06:43 +0300

    Add CloudinaryConfig.

--- src/main/java/com/debugArena/config/EventApiConfig.java
+++ src/main/java/com/debugArena/config/EventApiConfig.java
@@ -1,12 +1,10 @@
 package com.debugArena.config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
-@Setter
 @Configuration
 @ConfigurationProperties(prefix = "event.api")
 public class EventApiConfig {
