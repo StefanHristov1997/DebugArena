452cb604a8a1ac47cca8c937cb12a9ac6dec61d1
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-08-01 13:58:03 +0300

    Refactoring event service getEvents method.

--- src/main/java/com/debugArena/service/impl/EventServiceImpl.java
+++ src/main/java/com/debugArena/service/impl/EventServiceImpl.java
@@ -3,12 +3,17 @@ package com.debugArena.service.impl;
 import com.debugArena.exeption.ObjectNotFoundException;
 import com.debugArena.exeption.ServerConnectionException;
 import com.debugArena.model.dto.binding.AddEventBindingModel;
+import com.debugArena.model.dto.binding.EventMainBindingModel;
 import com.debugArena.model.dto.view.EventDetailsInfoViewModel;
 import com.debugArena.model.dto.view.EventShortInfoViewModel;
+import com.debugArena.model.entity.UserEntity;
+import com.debugArena.repository.UserRepository;
 import com.debugArena.service.EventService;
 import com.debugArena.service.helpers.EventAPIServerStatusHelper;
+import org.modelmapper.ModelMapper;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
@@ -20,13 +25,17 @@ public class EventServiceImpl implements EventService {
 
     private final RestClient restClient;
     private final EventAPIServerStatusHelper eventAPIServerStatusHelper;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     public EventServiceImpl(
             RestClient restClient,
-            EventAPIServerStatusHelper eventAPIServerStatusHelper) {
+            EventAPIServerStatusHelper eventAPIServerStatusHelper, UserRepository userRepository, ModelMapper modelMapper) {
 
         this.restClient = restClient;
         this.eventAPIServerStatusHelper = eventAPIServerStatusHelper;
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -73,13 +82,24 @@ public class EventServiceImpl implements EventService {
 
         checkingServerStatus();
 
-        return restClient
+        List<EventMainBindingModel> response = restClient
                 .get()
                 .uri("api/events/all")
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
+
+        assert response != null;
+
+        return response.stream().peek(e -> {
+            UserEntity author = userRepository.findByEmail(e.getAuthorEmail())
+                    .orElseThrow(() ->
+                            new UsernameNotFoundException("User with email: " + e.getAuthorEmail() + " is not found"));
+
+            e.setAuthorImageURL(author.getImageURL());
+
+        }).map(e -> modelMapper.map(e, EventShortInfoViewModel.class)).toList();
     }
 
     @Override
